f45f0ebe9d050ca7c7f1f07ca5630da2eea1ccc5
zeevNoiman <zevnoiman@gmail.com>
2020-10-27 23:20:15 +0300

    recursive algorithm to longest common prefix

--- LongestCommonPrefix.js
+++ LongestCommonPrefix.js
@@ -2,25 +2,55 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    if (strs.length <=0){
-        return '';
-    }
-    const lenghtOfWords = strs.map(word => 
-                                word.length
-                                )
-    const minLength = lenghtOfWords.sort((a, b) => a - b)[0];
 
-    var firstLettersSorted;
-    var i = 0
-    for(i; i < minLength; i++){
-        const firstLetters = strs.map(word => word[i] )
-        firstLettersSorted = firstLetters.sort()
-        if(firstLettersSorted[0] != firstLettersSorted[firstLettersSorted.length - 1]){
-            break;
-        }
+
+// var longestCommonPrefix = function(strs) {
+//     if (strs.length <=0){
+//         return '';
+//     }
+//     const lenghtOfWords = strs.map(word => 
+//                                 word.length
+//                                 )
+//     const minLength = lenghtOfWords.sort((a, b) => a - b)[0];
+    
+//     console.log(minLength);
+
+//     var firstLettersSorted;
+//     var i = 0
+//     for(i; i < minLength; i++){
+//         const firstLetters = strs.map(word => word[i] )
+//         firstLettersSorted = firstLetters.sort()
+//         if(firstLettersSorted[0] != firstLettersSorted[firstLettersSorted.length - 1]){
+//             break;
+//         }
+//     }
+//     return strs[0].substring(0, i);
+// };
+
+
+//Recursive Algorithm
+var longestCommonPrefix = function(strs) {
+    if(strs.length == 0){
+        return "";
     }
-    return strs[0].substring(0, i);
+
+    if (strs.length == 1){
+        return strs[0];
+    }
+
+    const firstWord = strs[0];
+    const secondWord = strs[1];
+    const minLength = firstWord.length < secondWord.length ? firstWord.length : secondWord.length;
+    
+    var i = 0
+    for( i ; i < minLength; i++){
+        if(firstWord[i]!=secondWord[i]) break;
+    }
+
+    const commonWord = firstWord.substring(0, i)
+    strs.splice(0,2,commonWord);
+    
+    return longestCommonPrefix(strs)
 };
 
 console.log(longestCommonPrefix(["flower","flow","flight"]));
