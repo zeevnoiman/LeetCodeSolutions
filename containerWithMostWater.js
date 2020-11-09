d25276eb615080739f8b0c2ead6224165ad8ed63
zeevNoiman <zevnoiman@gmail.com>
2020-11-09 12:51:22 +0200

    container with most water optmized with greedy algorithm

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,22 +1,31 @@
-//bruteForce
-var maxArea = function(height) {
-    var maxWater = 0;
-    for (var i = 0; i<height.length; i++){
-        for (var j=0; j<height.length; j++){
-           var water = (j - i) * Math.min(height[i], height[j])
-           if(water > maxWater){
-               maxWater = water
-           }
-        }
-    }
-    return maxWater;
-};
+// //bruteForce
+// var maxArea = function(height) {
+//     var maxWater = 0;
+//     for (var i = 0; i<height.length; i++){
+//         for (var j=0; j<height.length; j++){
+//            var water = (j - i) * Math.min(height[i], height[j])
+//            if(water > maxWater){
+//                maxWater = water
+//            }
+//         }
+//     }
+//     return maxWater;
+// };
 
 var maxArea = function(height) {
-    var newHeight = new Array(10);
-    for (var i = 0; i<height.length; i++){
-        newHeight[height[i]] = []
+    var i = 0;
+    var j = height.length -1;
+
+    var maxWater = 0;
+    while(i < j){
+        var smallerWaterIndex = height[i] < height[j] ? i : j;
+        var waterArea = (j - i) * height[smallerWaterIndex];
+        if(waterArea > maxWater){
+            maxWater = waterArea;
+        }
+        height[i] < height[j] ? i++ : j--;
     }
+
     return maxWater;
 };
 
