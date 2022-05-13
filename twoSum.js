9888ec3f755f27161fd67e494ac4e95f1046f7f7
zeevnoiman <zevnoiman@gmail.com>
2022-05-13 14:03:08 +0300

    add twoSum algorithm

---
+++ twoSum.js
@@ -0,0 +1,30 @@
+const twoSum = (nums, target) => {
+    nums = nums.sort((a, b) => a - b);
+    let left = 0
+    let right = nums.length - 1
+
+    const result = []
+    while(left < right) {
+        if (left > 0 && nums[left] === nums[left -1]) {
+            left++;
+            continue
+        }
+
+        if (nums[left] + nums[right] === target) {
+            result.push([nums[left], nums[right]])
+            left++
+            continue
+        }
+        if (nums[left] + nums[right] < target) {
+            left++
+            continue
+        }
+        right--
+    }
+
+    return result
+}
+
+const example = [2, 0, 5, 2, -2, 4, 3, 3, 1, 7]
+
+console.log(twoSum(example, 4))
