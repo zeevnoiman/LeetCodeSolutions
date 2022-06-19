55086000dc9e4cfe7a35fc6598fe6f2f2d277ced
zeevnoiman <zevnoiman@gmail.com>
2022-06-19 09:42:45 +0300

    remove nth item from the end in linked list

---
+++ removeNthFromEndLinkedList.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let revertedListHead = revertList(head)
+
+    if (n === 1) {
+        revertedListHead = revertedListHead.next
+        if (!revertedListHead) {
+            return revertedListHead
+        }
+        const revertedAgain = revertList(revertedListHead)
+        return revertedAgain
+    }
+
+    let newHead = new ListNode(0, revertedListHead)
+    let count = 1
+    while (count != n - 1) {
+        revertedListHead = revertedListHead.next
+        count++
+    }
+
+    revertedListHead.next = revertedListHead.next.next
+    const revertedAgain = revertList(newHead.next)
+    return revertedAgain
+};
+
+const revertList = (head) => {
+    let invertedNextNode = new ListNode(head.val)
+    while (head.next) {
+        invertedNextNode = new ListNode(head.next.val, invertedNextNode)
+        head = head.next
+    }
+
+    return invertedNextNode
+}
